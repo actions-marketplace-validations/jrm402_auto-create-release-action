c06f0245712a825091cf9afe5b133aacc0620665
Jimmy Metz <35279188+jrm402@users.noreply.github.com>
2026-02-24 19:35:04 -0700

    feat: check for package and changelog file paths

--- README.md
+++ README.md
@@ -18,7 +18,8 @@ The following inputs are available. All inputs are optional.
 - `title`: Title of the release (The slug `$version` is replaced by the version detected in `package.json`). Default `v$version`
 - `tag`: Name of the release tag (The slug `$version` is replaced by the version detected in `package.json`). Default `v$version`
 - `draft`: Is this a draft release or not. Default `false`
-- `changelog`: The path to the CHANGELOG.md file (including the file name). Default `CHANGELOG.md`
+- `package-file`: The path to the `package.json` file. Default `package.json`
+- `changelog-file`: The path to the CHANGELOG file (including the file name). Default `CHANGELOG.md`
 - `changelog-header-regexp`: The regular expression string a CHANGELOG line must match to be considered a version header. This MUST match a version group in the string. This group will be checked against the version found in package.json. Default `^## v(\d+\.\d+\.\d+(\-.+)*)`
 
 ### Outputs
@@ -55,7 +56,8 @@ jobs:
           title: "v$version" # Optional
           tag: "v$version" # Optional
           draft: false # Optional
-          changelog: "CHANGELOG.md" # Optional
+          package-file: "package.json" # Optional
+          changelog-file: "CHANGELOG.md" # Optional
           changelog-header-regexp: "^## v(\d+\.\d+\.\d+)" # Optional
         env:
           GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
--- src/main.js
+++ src/main.js
@@ -11,13 +11,15 @@ async function run() {
     const rawTitle = core.getInput("title");
     const rawTag = core.getInput("tag");
     const rawDraft = core.getInput("draft");
-    const rawChangelog = core.getInput("changelog");
+    const rawPackageFile = core.getInput("package-file");
+    const rawChangelog = core.getInput("changelog-file");
     const rawChangelogHeaderRegexp = core.getInput("changelog-header-regexp");
 
     const title = !rawTitle ? "v$version" : rawTitle;
     const tag = !rawTag ? "v$version" : rawTag;
     const draft =
       (rawDraft == null ? "false" : rawDraft).toLowerCase() === "true";
+    const packageFile = rawPackageFile || "package.json";
     const changelog = rawChangelog ? "CHANGELOG.md" : rawChangelog;
     const changelogHeaderRegexp = rawChangelogHeaderRegexp
       ? "^## v(\\d+\\.\\d+\\.\\d+(\\-.+)*)"
@@ -33,14 +35,14 @@ async function run() {
       commitId = commit.id;
     }
 
-    const pkgInfo = await getCommitInfo(token, "package.json", commitId);
+    const pkgInfo = await getCommitInfo(token, packageFile, commitId);
     const clInfo = await getCommitInfo(token, changelog, commitId);
 
     // check package.json file
     if (pkgInfo == null) {
-      return setFailed("package.json file could not be found.", true);
+      return setFailed(`${packageFile} file could not be found.`, true);
     } else if (pkgInfo.length === 0) {
-      return setFailed("package.json file is blank.", true);
+      return setFailed(`${packageFile} file is blank.`, true);
     }
 
     // check changelog file
@@ -53,7 +55,7 @@ async function run() {
     // load version
     const _version = /"version":\s*"(.+)"/.exec(pkgInfo);
     if (_version == null || !_version[1]) {
-      return setFailed("Version was not found in package.json.", true);
+      return setFailed(`Version was not found in ${packageFile}.`, true);
     }
     const version = _version[1];
 
